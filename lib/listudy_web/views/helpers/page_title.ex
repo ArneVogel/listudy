753218984a1860ca11d0a88aece1002b2b58771a
ArneVogel <arnevogel96@gmail.com>
2020-06-29 19:20:07 +0200

    improve homepage title

--- lib/listudy_web/views/helpers/page_title.ex
+++ lib/listudy_web/views/helpers/page_title.ex
@@ -9,7 +9,7 @@ defmodule ListudyWeb.PageTitle do
   defp put_suffix(nil), do: @suffix
   defp put_suffix(title), do: title <> " - " <> @suffix
 
-  defp get(%{ view_module: PageView}), do: gettext("Homepage")
+  defp get(%{ view_module: PageView}), do: gettext("Improve you chess game with spaced repetition")
   defp get(%{ view_module: Elixir.ListudyWeb.StudyView, view_template: "show.html", "study": study}), do: study.title
   defp get(%{ view_module: Elixir.ListudyWeb.PostView, view_template: "show.html", "post": post}), do: post.title
   defp get(%{ live_module: StudySearchLive}), do: gettext("Search Studies")
