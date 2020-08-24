ddd7acee4080bdcbd7b39fa73a42a06b035f4306
ArneVogel <arnevogel96@gmail.com>
2020-08-24 14:23:55 +0200

    fix hreflang tag ending

--- lib/listudy_web/views/helpers/seo.ex
+++ lib/listudy_web/views/helpers/seo.ex
@@ -20,7 +20,7 @@ defmodule ListudyWeb.Seo do
       true ->
         Enum.reduce @languages, "", fn lang, acc ->
           url = clean("#{@domain}/#{lang}/#{non_language_path}")
-          acc <> "<link rel=\"alternate\" hreflang=\"#{lang}\" href=\"#{url}\" \\>" <> "\n"
+          acc <> "<link rel=\"alternate\" hreflang=\"#{lang}\" href=\"#{url}\" />" <> "\n"
         end
       _ ->
         ""
