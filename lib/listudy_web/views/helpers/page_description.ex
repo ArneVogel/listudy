dd954226afe7fcd6ea15edf7b707bf85f64488f3
ArneVogel <arnevogel96@gmail.com>
2020-08-27 21:53:12 +0200

    add descriptions for event, motif, player and opening public pages

--- lib/listudy_web/views/helpers/page_description.ex
+++ lib/listudy_web/views/helpers/page_description.ex
@@ -7,6 +7,20 @@ defmodule ListudyWeb.PageDescription do
   defp get(%{ view_module: Elixir.ListudyWeb.StudyView, view_template: "show.html", "study": study}), do: trim(study.description)
   defp get(%{ view_module: Elixir.ListudyWeb.PostView, view_template: "show.html", "post": post}), do: trim(hd(String.split(post.body, "\n")))
   defp get(%{ live_module: StudySearchLive}), do: gettext("Search for studies to train specific openings")
+
+  defp get(%{ view_module: Elixir.ListudyWeb.MotifView, view_template: "public.html", "motif": motif}) do
+    gettext("Get better at %{name} tactics with our huge library of puzzles taken from real games. Explore our free collection of %{name} tactics.", name: motif.name)
+  end
+  defp get(%{ view_module: Elixir.ListudyWeb.EventView, view_template: "public.html", "event": event}) do
+    gettext("Play tactics from the event %{}. Are you better than the professionals?", name: event.name)
+  end
+  defp get(%{ view_module: Elixir.ListudyWeb.PlayerView, view_template: "public.html", "player": player}) do
+    gettext("Explore our collection of tactics taken from games by %{name}.", name: player.name)
+  end
+  defp get(%{ view_module: Elixir.ListudyWeb.OpeningView, view_template: "public.html", "opening": opening}) do
+    gettext("Get better in the %{name} opening. Study the opening and learn from traps and tactics collected from real games.", name: opening.name)
+  end
+
   defp get(_), do: gettext("Improve your chess game on Listudy")
 
   defp trim(s) do
