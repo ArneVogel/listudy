6e6d71a98087e60737c4ba7703f0ef7cb2d96156
ArneVogel <arnevogel96@gmail.com>
2020-08-31 13:22:26 +0200

    add description to daily puzzle page

--- lib/listudy_web/views/helpers/page_description.ex
+++ lib/listudy_web/views/helpers/page_description.ex
@@ -4,6 +4,10 @@ defmodule ListudyWeb.PageDescription do
 
   def page_description(assigns), do: assigns |> get 
 
+  defp get(%{ view_module: Elixir.ListudyWeb.TacticView, view_template: "daily.html"}) do
+    gettext("Solve the Daily Chess Puzzle! Updated every day from our list of best puzzles.")
+  end
+
   defp get(%{ view_module: Elixir.ListudyWeb.StudyView, view_template: "show.html", "study": study}), do: trim(study.description)
   defp get(%{ view_module: Elixir.ListudyWeb.PostView, view_template: "show.html", "post": post}), do: trim(hd(String.split(post.body, "\n")))
   defp get(%{ live_module: StudySearchLive}), do: gettext("Search for studies to train specific openings")
