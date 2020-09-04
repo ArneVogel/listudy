d8e78d4fc240fb476646cef7f86823465a6217d4
ArneVogel <arnevogel96@gmail.com>
2020-09-04 16:52:56 +0200

    add logged in/logged out test case for homepage

--- test/listudy_web/controllers/page_controller_test.exs
+++ test/listudy_web/controllers/page_controller_test.exs
@@ -1,6 +1,17 @@
 defmodule ListudyWeb.PageControllerTest do
   use ListudyWeb.ConnCase
 
+  setup %{conn: conn} do
+    user = %Listudy.Users.User{
+      username: "Arne",
+      id: 1
+    }
+
+    authed_conn = Pow.Plug.assign_current_user(conn, user, [])
+
+    {:ok, conn: conn, authed_conn: authed_conn}
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 302) =~ "redirected"
@@ -9,6 +20,17 @@ defmodule ListudyWeb.PageControllerTest do
   test "GET /en", %{conn: conn} do
     conn = get(conn, "/en")
     assert html_response(conn, 200) =~ "Listudy"
+  end
+
+  test "logged in GET /en", %{authed_conn: conn} do
+    conn = get(conn, "/en")
+    assert html_response(conn, 200) =~ "registration/edit"
+  end
+
+  test "logged out GET /en", %{conn: conn} do
+    conn = get(conn, "/en")
+    assert !(html_response(conn, 200) =~ "registration/edit")
+    assert html_response(conn, 200) =~ "Register"
   end
 
   test "GET /de", %{conn: conn} do
