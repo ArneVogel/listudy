7dc66799cc6fe422c5d4b36d47d401fe598de667
ArneVogel <arnevogel96@gmail.com>
2020-09-11 10:15:45 +0200

    add openings to sitemap

--- lib/listudy_web/controllers/sitemap_controller.ex
+++ lib/listudy_web/controllers/sitemap_controller.ex
@@ -7,9 +7,10 @@ defmodule ListudyWeb.SitemapController do
   def index(conn, _params) do
     studies = Studies.get_all_public_studies()
     posts = Content.public_posts()
+    openings = Listudy.Openings.list_openings()
 
     conn
     |> put_resp_content_type("text/xml")
-    |> render("index.xml", studies: studies, posts: posts)
+    |> render("index.xml", studies: studies, posts: posts, openings: openings)
   end
 end
