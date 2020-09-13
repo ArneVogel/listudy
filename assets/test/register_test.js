62b35671755c4fad1420056e0835d0b2a4e7827a
ArneVogel <arnevogel96@gmail.com>
2020-09-13 23:28:23 +0200

    fix register test script

--- assets/test/register_test.js
+++ assets/test/register_test.js
@@ -6,7 +6,8 @@ Scenario("invalid register", (I) => {
     I.fillField("Username", "a");
     I.fillField("Email", "invalid");
     I.fillField("Password", "abcdefg");
-    I.click("Register", "//html/body/main/form/div/button");
+    I.click("Register", "//html/body/main/div/div[1]/form/div/button");
+    I.see("should be at least 3 character(s)");
     I.see("has invalid format");
     I.see("should be at least 8 character(s)");
 });
