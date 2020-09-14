22f335f3921a73317bff155f42e047eab194f856
ArneVogel <arnevogel96@gmail.com>
2020-09-14 13:33:16 +0200

    more tests

--- assets/test/register_test.js
+++ assets/test/register_test.js
@@ -13,14 +13,17 @@ Scenario("invalid register", (I) => {
     I.see("should be at least 8 character(s)");
 });
 
-Scenario("valid register", (I) => {
+Scenario("valid register + deletion", (I) => {
     I.amOnPage("/en");
     I.click("Register");
     I.fillField("Username", "abcd");
     I.fillField("Email", "valid@listudy.org");
     I.fillField("Password", "abcdefgf");
-    I.fillField("Password", "abcdefgf");
     I.fillField("Password confirmation", "abcdefgf");
     I.click("Register", "//html/body/main/div/div[1]/form/div/button");
     I.see("Welcome to Listudy!");
+    I.amOnPage("/registration/edit");
+    I.see("Edit profile");
+    I.click("Close Account");
+    I.see("Your account has been deleted. Sorry to see you go!");
 });
