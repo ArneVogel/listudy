dee50b3d4cfcb2b781038d923a988d914cb97710
ArneVogel <arnevogel96@gmail.com>
2020-09-16 14:09:49 +0200

    test

--- lib/listudy_web/plugs/stockfish.ex
+++ lib/listudy_web/plugs/stockfish.ex
@@ -6,7 +6,5 @@ defmodule ListudyWeb.Plugs.Stockfish do
   def call(conn, _default) do
     conn = put_resp_header conn, "cross-origin-embedder-policy", "require-corp"
     conn = put_resp_header conn, "cross-origin-opener-policy", "same-origin"
-    conn = put_resp_header conn, "Cross-Origin-Opener-Policy", "same-origin"
-    conn = put_resp_header conn, "Cross-Origin-Embedder-Policy", "require-corp"
   end
 end
