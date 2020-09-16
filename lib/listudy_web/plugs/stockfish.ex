82cad2cc73bd4b674482ed2faba1267df00f05cc
ArneVogel <arnevogel96@gmail.com>
2020-09-16 13:56:12 +0200

    double headers for chrome

--- lib/listudy_web/plugs/stockfish.ex
+++ lib/listudy_web/plugs/stockfish.ex
@@ -4,7 +4,9 @@ defmodule ListudyWeb.Plugs.Stockfish do
   def init(default), do: default
 
   def call(conn, _default) do
-    put_resp_header conn, "Cross-Origin-Embedder-Policy", "require-corp"
-    put_resp_header conn, "Cross-Origin-Opener-Policy", "same-origin"
+    conn = put_resp_header conn, "cross-origin-embedder-policy", "require-corp"
+    conn = put_resp_header conn, "cross-origin-opener-policy", "same-origin"
+    conn = put_resp_header conn, "Cross-Origin-Opener-Policy", "same-origin"
+    conn = put_resp_header conn, "Cross-Origin-Embedder-Policy", "require-corp"
   end
 end
