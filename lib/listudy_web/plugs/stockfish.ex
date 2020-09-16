af659abcd7443035ae73365b6d8ed988a304b74c
ArneVogel <arnevogel96@gmail.com>
2020-09-16 13:33:41 +0200

    capitalize extra stockfish headers

--- lib/listudy_web/plugs/stockfish.ex
+++ lib/listudy_web/plugs/stockfish.ex
@@ -4,7 +4,7 @@ defmodule ListudyWeb.Plugs.Stockfish do
   def init(default), do: default
 
   def call(conn, _default) do
-    conn = put_resp_header conn, "cross-origin-embedder-policy", "require-corp"
-    conn = put_resp_header conn, "cross-origin-opener-policy", "same-origin"
+    put_resp_header conn, "Cross-Origin-Embedder-Policy", "require-corp"
+    put_resp_header conn, "Cross-Origin-Opener-Policy", "same-origin"
   end
 end
