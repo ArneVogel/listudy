5e03ad20283ba4c1133a7dcff3e17f9a7d5f4b9b
ArneVogel <arnevogel96@gmail.com>
2020-09-19 23:51:13 +0200

    add LiveDashboard to /admin

--- lib/listudy_web/router.ex
+++ lib/listudy_web/router.ex
@@ -1,6 +1,7 @@
 defmodule ListudyWeb.Router do
   use ListudyWeb, :router
   use Pow.Phoenix.Router
+  import Phoenix.LiveDashboard.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -56,6 +57,8 @@ defmodule ListudyWeb.Router do
     resources "/tactics", TacticController
 
     resources "/blind_tactics", BlindTacticController
+
+    live_dashboard "/dashboard", metrics: ListudyWeb.Telemetry
   end
 
   scope "/:locale", ListudyWeb do
@@ -141,26 +144,5 @@ defmodule ListudyWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :domain
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", ListudyWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: ListudyWeb.Telemetry
-    end
   end
 end
