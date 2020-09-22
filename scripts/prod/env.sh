a6b93365820e78742d9be77bdbdd19d400db9d31
ArneVogel <arnevogel96@gmail.com>
2020-09-22 22:00:17 +0200

    add valid env variables to start dev server

--- scripts/prod/env.sh
+++ scripts/prod/env.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # environment variables for production
+
 export PORT=4000
+# these variables are only read for prod
 export MIX_ENV=prod
-export SECRET_KEY_BASE=SECRET
-export DATABASE_URL=abc
+# generate your own secret with: mix phx.gen.secret
+export SECRET_KEY_BASE=CHANGE_THIS_Ax1nJt52LzAPVFNUc9Wp7Ux6JF0C4toh/JdnK6iXZQgR5hsXH4SCTBsC/COjEsj/
+# database url for my dev server
+export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/listudy_dev
