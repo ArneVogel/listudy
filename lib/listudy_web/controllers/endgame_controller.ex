7a2a0d07907c65657d99bb39fac8b71c3a5a880c
ArneVogel <arnevogel96@gmail.com>
2020-09-25 14:48:29 +0200

    add Capablancas Chess Fundamentals endgames

--- lib/listudy_web/controllers/endgame_controller.ex
+++ lib/listudy_web/controllers/endgame_controller.ex
@@ -2,7 +2,7 @@ defmodule ListudyWeb.EndgameController do
   use ListudyWeb, :controller
 
   defp endgames do
-    %{"basic" => basic()}
+    [basic: basic(), "chess-fundamentals": fundamentals()]
   end
 
   def index(conn, _params) do
@@ -10,13 +10,13 @@ defmodule ListudyWeb.EndgameController do
   end
 
   def chapter(conn, %{"chapter" => chapter}) do
-    render(conn, "chapter.html", chapter: endgames()[chapter], slug: chapter)
+    render(conn, "chapter.html", chapter: endgames()[String.to_atom(chapter)], slug: chapter)
   end
 
   def game(conn, %{"chapter" => chapter_slug, "subchapter" => subchapter_slug, "game" => game}) do
     {game_id, _} = Integer.parse(game)
     index = game_id - 1
-    chapter = endgames()[chapter_slug]
+    chapter = endgames()[String.to_atom(chapter_slug)]
     subchapter = chapter[:endgames][String.to_atom(subchapter_slug)]
     game_list = subchapter[:games]
     game = Enum.at(game_list, index)
@@ -102,6 +102,79 @@ defmodule ListudyWeb.EndgameController do
             ["8/6k1/8/3BK3/4N3/8/8/8 w - - 0 1", "w"],
             ["8/8/8/3k4/8/8/8/3BNK2 w - - 0 1", "w"],
             ["8/8/8/3k4/8/8/8/3NBK2 w - - 0 1", "w"]
+          ]
+        }
+      ]
+    }
+  end
+
+  defp fundamentals() do
+    %{
+      name: gettext("Capablancas Chess Fundamentals"),
+      description:
+        gettext("Play the endgames from Capablancas renowned book \"Chess Fundamentals\"."),
+      body:
+        gettext(
+          "In these chapters are the endgames from Capablancas chess book \"Chess Fundamentals\". Learn the most important endgame fundamentals such as the opposition, creating a passed pawn and checkmating ideas."
+        ),
+      endgames: [
+        "simples-mates": %{
+          name: gettext("Simple Mates"),
+          games: [
+            ["7k/8/8/8/8/8/8/R6K w - - 0 1", "w"],
+            ["8/8/8/4k3/8/8/8/4K2R w - - 0 1", "w"],
+            ["7k/8/8/8/8/8/8/2B1KB2 w - - 0 1", "w"],
+            ["8/8/8/4k3/8/8/8/4K2Q w - - 0 1", "w"]
+          ]
+        },
+        "pawn-promotion": %{
+          name: gettext("Pawn Promotion"),
+          games: [
+            ["8/8/5k2/8/5K2/8/4P3/8 w - - 0 1", "p"],
+            ["8/3kp3/8/4K3/8/8/8/8 w - - 0 1", "d"]
+          ]
+        },
+        "pawn-endings": %{
+          name: gettext("Pawn Endings"),
+          games: [
+            ["5k2/6p1/4K1P1/5P2/8/8/8/8 w - - 0 1", "w"],
+            ["8/6p1/3k4/8/3K1PP1/8/8/8 w - - 0 1", "w"],
+            ["8/6pp/3k4/8/3K1PPP/8/8/8 w - - 0 1", "w"],
+            ["8/p6p/3k4/8/3K4/8/P5PP/8 w - - 0 1", "w"],
+            ["8/p5p1/7p/6k1/8/7K/PP5P/8 w - - 0 1", "d"],
+            ["8/6kp/8/4K3/8/6P1/7P/8 w - - 0 1", "w"]
+          ]
+        },
+        "passed-pawn": %{
+          name: gettext("Obtaining a Passed Pawn"),
+          games: [
+            ["8/ppp4k/8/PPP5/8/8/7K/8 w - - 0 1", "w"],
+            ["8/p5kp/8/1P6/8/1K6/P7/8 w - - 0 1", "w"]
+          ]
+        },
+        "the-opposition": %{
+          name: gettext("The Opposition"),
+          games: [
+            ["8/8/4k3/1p5p/1P2K2P/8/8/8 w - - 0 1", "d"],
+            ["4k3/8/8/1p5p/1P5P/8/8/4K3 w - - 0 1", "w"],
+            ["8/8/8/4p1p1/8/5P2/6K1/3k4 w - - 0 1", "d"]
+          ]
+        },
+        "values-of-knight-and-bishop": %{
+          name: gettext("The Relative Value of Knight and Bishop"),
+          games: [
+            ["k7/8/1K1NN3/8/8/8/8/8 w - - 0 1", "d"],
+            ["k7/8/1K1N4/7p/7N/8/8/8 w - - 0 1", "w"],
+            ["8/7p/8/2b3k1/8/8/8/7K w - - 0 1", "d"],
+            ["8/8/5Np1/8/8/7p/5K2/7k w - - 0 1", "w"]
+          ]
+        },
+        "queen-against-rook": %{
+          name: gettext("Queen against Rook"),
+          games: [
+            ["1k6/1r6/2K5/Q7/8/8/8/8 w - - 0 1", "w"],
+            ["8/k7/2K5/4Q3/8/8/8/1r6 w - - 0 1", "w"],
+            ["8/5rk1/8/5Q1K/8/8/8/8 w - - 0 1", "w"]
           ]
         }
       ]
