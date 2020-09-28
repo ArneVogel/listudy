60a3b86b0b2db1673345cd4af83b50238e853aaf
ArneVogel <arnevogel96@gmail.com>
2020-09-28 15:24:21 +0200

    add a endgame test

---
+++ assets/test/endgames_test.js
@@ -0,0 +1,11 @@
+Feature("Endgames");
+
+Scenario("Visit a endgame", (I) => {
+    I.amOnPage("/en");
+    I.click("Endgames");
+    I.see("Endgames");
+    I.click("Learn");
+    I.see("Queen");
+    I.click("1");
+    I.see("White to play and win");
+});
