f6e7a82c2d0d1b7efbe1883dfa89166e545b64f5
ArneVogel <arnevogel96@gmail.com>
2020-10-06 13:02:21 +0200

    make halloween styling conditional

--- lib/listudy_web/views/layout_view.ex
+++ lib/listudy_web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule ListudyWeb.LayoutView do
   use ListudyWeb, :view
+
+  def is_halloween(date) do
+    {{_, month, day}, {hour, _, _}} = date
+    month == 10 and day in 27..31 or month == 11 and day == 1 and hour in 0..6
+  end
+
+  def is_halloween do
+    is_halloween(:calendar.universal_time())
+  end
 end
