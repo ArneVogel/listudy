d0116cde4721c224404f102da30d3fe629287a11
ArneVogel <arnevogel96@gmail.com>
2020-11-25 12:35:12 +0100

    Make required study form inputs more obvious (#24)
    * update dependencies
    
    * make required study form inputs more obvious

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Listudy.MixProject do
   def application do
     [
       mod: {Listudy.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :mnesia]
     ]
   end
 
