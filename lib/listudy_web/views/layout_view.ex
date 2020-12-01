e651f6e4590a538815be9973b085a693c252feb4
ArneVogel <arnevogel96@gmail.com>
2020-12-01 17:18:10 +0100

    xmas css

--- lib/listudy_web/views/layout_view.ex
+++ lib/listudy_web/views/layout_view.ex
@@ -6,7 +6,16 @@ defmodule ListudyWeb.LayoutView do
     (month == 10 and day in 27..31) or (month == 11 and day == 1 and hour in 0..6)
   end
 
+  def is_xmas(date) do
+    {{_, month, day}, {_, _, _}} = date
+    month == 12 and day in 22..26
+  end
+
   def is_halloween do
     is_halloween(:calendar.universal_time())
+  end
+
+  def is_xmas do
+    is_xmas(:calendar.universal_time())
   end
 end
