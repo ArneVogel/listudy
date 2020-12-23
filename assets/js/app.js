fe9e1f3034c164059cf2e78f68e8f5db05af1083
ArneVogel <arnevogel96@gmail.com>
2020-12-23 23:09:20 +0100

    remove share links

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,33 +30,3 @@ if (toggle != null) {
         document.documentElement.setAttribute('data-theme', theme);
     }
 }
-
-/*
- * Setup share links if an element with id "share" exists on the page
- */
-function social_links() {
-    let div = document.getElementById("share");
-    if (div == undefined) {
-        return;
-    }
-    let url = window.location.href;
-    let sites = ["twitter", "reddit", "facebook"];
-    let urls = [`https://twitter.com/intent/tweet?text=${url}`, `https://www.reddit.com/submit?url=${url}`, `https://www.facebook.com/sharer/sharer.php?u=${url}`];
-
-    for (let i = 0; i < sites.length; ++i) {
-        let button = document.createElement("button");
-        button.textContent = i18n.share_on + " " + sites[i];
-
-        let link = document.createElement("a");
-        link.href = urls[i];
-        link.rel = "nofollow noopener noreferrer";
-        link.appendChild(button);
-        div.appendChild(link);
-    }
-}
-
-const load = () => {
-    social_links();
-}
-
-window.onload = load;
