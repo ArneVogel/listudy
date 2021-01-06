28148452ff7796e06fc1f3d9a3e655cf86f7ea73
ArneVogel <arnevogel96@gmail.com>
2021-01-06 23:56:41 +0100

    adjust blind tactics test case

--- assets/test/blind_tactics_test.js
+++ assets/test/blind_tactics_test.js
@@ -2,6 +2,6 @@ Feature("blind tactics");
 
 Scenario("visit blind tactic", (I) => {
     I.amOnPage("/en/blind-tactics");
-    I.see("e6 h6 Qf3 Qe8");
+    I.see("7. e6 h6 8. Qf3 Qe8");
     I.seeElement({xpath: "//html/body/main/div/div/div[1]/div/cg-helper/cg-container"})
 });
