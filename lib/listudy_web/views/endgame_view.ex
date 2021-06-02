dd2e49302f1f6a57ca03e58b06b52906c4729da6
ArneVogel <arnevogel96@gmail.com>
2021-06-02 12:15:16 +0200

    Use prerender of endgame tactics
    Hint to the browser what the next url of endgame tactics will be. This
    might improve the speed in which the next page loads.

--- lib/listudy_web/views/endgame_view.ex
+++ lib/listudy_web/views/endgame_view.ex
@@ -1,3 +1,10 @@
 defmodule ListudyWeb.EndgameView do
   use ListudyWeb, :view
+
+  def next_button(url) do
+    """
+    <a href="#{url}"><button id="next" class="hidden continue_button">#{dgettext("endgame", "Next") }</button></a>
+    <link href="#{url}" rel="prerender"/>
+    """
+  end
 end
