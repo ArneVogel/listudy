89b812bf51bce040e5339908cb1d20f83c67e03b
ArneVogel <arnevogel96@gmail.com>
2022-08-06 18:36:15 +0200

    migrate to Config

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :listudy,
   ecto_repos: [Listudy.Repo]
@@ -56,4 +56,4 @@ config :arc,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
